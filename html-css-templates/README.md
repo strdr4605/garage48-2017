cd903d1123be86a4ae3d644a56a29173c08044c3
strdr4605 <str.dr4605@gmail.com>
2017-02-18 11:17:26 +0200

    Init2 html-css-template (gulp) project

--- html-css-templates/README.md
+++ html-css-templates/README.md
@@ -1,4 +1,2 @@
-# Personal-Site. First task at Yopeso Front-end Internship
-## Run these commands to build the project
-1. `npm install`
-2. `gulp`
+# Sticky-Footer 
+Example using flexbox css
--- html-css-templates/gulpfile.js
+++ html-css-templates/gulpfile.js
@@ -2,50 +2,13 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
-var browserSync = require('browser-sync').create();
-var concat = require('gulp-concat');
-
-gulp.task('scripts', function() {
-  return gulp.src('./assets/src/js/*.js')
-    .pipe(concat('my-scripts.js'))
-    .pipe(gulp.dest('./assets/dist/js/'));
-});
-
-gulp.task('scripts:watch', function () {
-  gulp.watch('./assets/src/js/*.js', ['scripts']);
-});
-
-gulp.task('imagemin', () =>
-    gulp.src('./assets/src/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./assets/dist/images'))
-);
-
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-});
 
 gulp.task('sass', function () {
-  return gulp.src('./assets/src/sass/**/*.scss')
+  return gulp.src('./assets/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest('./assets/dist/css'));
+    .pipe(gulp.dest('./assets/css'));
 });
-
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./assets/src/sass/**/*.scss', ['sass']);
+  gulp.watch('./assets/sass/**/*.scss', ['sass']);
 });
-
-gulp.task('default', ['imagemin', 'sass', 'sass:watch', 'scripts', 'scripts:watch', 'browser-sync']);
-
-// autoprefixer gulp, imagemin gulp
