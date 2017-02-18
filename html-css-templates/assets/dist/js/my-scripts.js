9f15ae5ede2173f4034ddcd66847d0351c284f44
Sasha <s.sasha44@yahoo.com>
2017-02-18 16:06:14 +0200

    Working Dash Board

--- html-css-templates/assets/dist/js/my-scripts.js
+++ html-css-templates/assets/dist/js/my-scripts.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
     $(".menu-icon").click(function(){
         $(this).toggleClass("change");
-        if($("#dashboard").hasClass("active-dashboard")) {
-          $("#dashboard").addClass("inactive-dashboard");
-          $("#dashboard").removeClass("active-dashboard")
-        }
-        if(!$("#dashboard").hasClass("inactive-dashboard")) {
+        if(!($("#dashboard").hasClass("inactive-dashboard")) &&
+        !($("#dashboard").hasClass("active-dashboard"))) {
           $("#dashboard").addClass("active-dashboard");
         }
         else {
-          $("#dashboard").removeClass("inactive-dashboard");
+        if($("#dashboard").hasClass("inactive-dashboard")) {
+          $("#dashboard").addClass("active-dashboard");
+          $("#dashboard").removeClass("inactive-dashboard")
         }
+        else {
+          $("#dashboard").removeClass("active-dashboard")
+          $("#dashboard").addClass("inactive-dashboard");
+        }
+      }
     });
 });
